cc104a441a28dd8f441eccaec78b186e8d2b9076
swaroop-12 <swaroop.k.g49@gmail.com>
2024-08-30 16:51:12 +0530

    header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,27 @@
-
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dasboard from "./pages/Dasboard";
+import Projects from "./pages/Projects";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div>app</div>
-  )
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dasboard/>} />
+        <Route path="/projects" element={<Projects/>} />
+        <Route path="/signin" element={<SignIn/>} />
+        <Route path="/signup" element={<SignUp/>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/lib/esm/**/*.js',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [    
+        require('flowbite/plugin')
+  ],
 }
 
